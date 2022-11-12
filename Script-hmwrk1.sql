25ff538a9630933763a50c03f3cf6080d3bdfb23
Artem Sargsyan <sarkiman@icloud.com>
2022-11-12 12:10:33 +0100

    Fin hmwrk

---
+++ Script-hmwrk1.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS Album(
+	id SERIAL PRIMARY KEY,
+	artist_name VARCHAR(60) NOT NULL,
+	album_name VARCHAR(60) NOT NULL,
+	release_date date	
+);
+
+CREATE TABLE IF NOT EXISTS Track(
+	id SERIAL PRIMARY KEY REFERENCES Album(id),
+	duration INTEGER NOT NULL,
+	album_name VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Jenre(
+	id SERIAL PRIMARY KEY,
+	jenre_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Artist(
+	id SERIAL PRIMARY KEY,
+	artist_name VARCHAR(60) NOT NULL,
+	jenre INTEGER NOT NULL REFERENCES Jenre(id),
+	album VARCHAR(60)
+);
+
+CREATE TABLE IF NOT EXISTS Artists_Albums(
+	artist_id INTEGER REFERENCES Artist(id),
+	album_id INTEGER REFERENCES Album(id),
+	CONSTRAINT pk PRIMARY KEY (artist_id, album_id)
+);
+
+CREATE TABLE IF NOT EXISTS Digest(
+	id SERIAL PRIMARY KEY,
+	digest_name VARCHAR(50) NOT NULL,
+	release_date date NOT NULL,
+	track_id INTEGER NOT NULL REFERENCES TracK(id)
+);
+
+CREATE TABLE IF NOT EXISTS ArtistJenres(
+	artist_id INTEGER REFERENCES Artist(id),
+	jenre_id INTEGER REFERENCES Jenre(id),
+	CONSTRAINT pk1 PRIMARY KEY (artist_id, jenre_id)
+);
+	
